4d437d375060db7b962c3abf03138e41ac56f988
Oscar Monroy <oscarmonroy657@gmail.com>
2022-06-10 20:22:42 -0600

    Doughnut chart has been implemented correctly.

--- src/app/charts/pages/doughnut/doughnut.component.ts
+++ src/app/charts/pages/doughnut/doughnut.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ChartData, ChartEvent, ChartType, Color } from 'chart.js';
 
 @Component({
   selector: 'app-doughnut',
@@ -7,6 +8,16 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class DoughnutComponent implements OnInit {
+  public doughnutChartLabels: string[] = ['Download Sales', 'In-Store Sales', 'Mail-Order Sales'];
+  public doughnutChartData: ChartData<'doughnut'> = {
+    labels: this.doughnutChartLabels,
+    datasets: [
+      { data: [350, 450, 100], backgroundColor: ['#F58F51', '#D744FC', '#8149FC'], hoverBackgroundColor: ['#F58F51', '#D744FC', '#8149FC'], hoverBorderColor: 'white'},
+      // { data: [ 50, 150, 120 ] },
+      // { data: [ 250, 130, 70 ] }
+    ]
+  };
+  public doughnutChartType: ChartType = 'doughnut';
 
   constructor() { }
 
