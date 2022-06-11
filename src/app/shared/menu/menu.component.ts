f0b33c57163cbdaf39ca9f270d4a8dc77609333c
Oscar Monroy <oscarmonroy657@gmail.com>
2022-06-10 19:09:28 -0600

    Menu has been created, configurated and added.

--- src/app/shared/menu/menu.component.ts
+++ src/app/shared/menu/menu.component.ts
@@ -1,12 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 
+interface MenuItem {
+  path: string;
+  name: string;
+}
+
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styles: [
-  ]
+  styles: [`
+    li {
+      cursor: pointer;
+    }
+  `]
 })
 export class MenuComponent implements OnInit {
+  menu: MenuItem[] = [
+    {path: '/charts/bars', name: 'Bars'},
+    {path: '/charts/double-bars', name: 'Double bars'},
+    {path: '/charts/doughnut', name: 'Doughnut'},
+    {path: '/charts/doughnut-http', name: 'Doughnut (HTTP)'},
+  ]
 
   constructor() { }
 
