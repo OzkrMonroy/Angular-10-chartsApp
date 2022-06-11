03d76b48da94e482f456fdd733446ac0580e62b3
Oscar Monroy <oscarmonroy657@gmail.com>
2022-06-11 07:24:20 -0600

    The Doughnut chart with data from a server is working.

--- src/app/charts/pages/doughnut-http/doughnut-http.component.ts
+++ src/app/charts/pages/doughnut-http/doughnut-http.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ChartData, ChartEvent, ChartType, Color } from 'chart.js';
+import { ChartsService } from '../../services/charts.service';
 
 @Component({
   selector: 'app-doughnut-http',
@@ -7,10 +9,26 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class DoughnutHttpComponent implements OnInit {
+  public loading: boolean = true;
+  public doughnutChartLabels: string[] = [];
+  public doughnutChartData: ChartData<'doughnut'> = {
+    labels: this.doughnutChartLabels,
+    datasets: []
+  };
+  public doughnutChartType: ChartType = 'doughnut';
 
-  constructor() { }
+  constructor(private service: ChartsService) { }
 
   ngOnInit(): void {
+    this.service.getUsersToDoughnut().subscribe({
+      next: ({ labels, datasets }) => {
+        this.loading = false;
+        
+        this.doughnutChartLabels = [...labels];
+        this.doughnutChartData.labels = [...labels];
+        this.doughnutChartData.datasets.push({data: datasets})
+      }
+    })
   }
 
 }
--- src/app/charts/services/charts.service.ts
+++ src/app/charts/services/charts.service.ts
@@ -1,9 +1,26 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { delay, map, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ChartsService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getUsersSocialMedia(): Observable<any>{
+    return this.http.get('http://localhost:3000/grafica');
+  }
+  getUsersToDoughnut(): Observable<any>{
+    return this.getUsersSocialMedia().pipe(
+      delay(1500),
+      map(data => {
+        const labels = Object.keys(data);
+        const datasets: number[] = Object.values(data);
+
+        return { labels, datasets }
+      })
+    )
+  }
 }
