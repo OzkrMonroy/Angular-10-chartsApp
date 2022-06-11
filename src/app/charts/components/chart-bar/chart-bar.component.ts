9a025e15f06c8b77e49a6230820b0752c0e02814
Oscar Monroy <oscarmonroy657@gmail.com>
2022-06-10 20:11:53 -0600

    Component to render bars or horizontal bars is ready.

--- src/app/charts/components/chart-bar/chart-bar.component.ts
+++ src/app/charts/components/chart-bar/chart-bar.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import { ChartConfiguration, ChartData, ChartDataset, ChartEvent, ChartType } from 'chart.js';
+import { BaseChartDirective } from 'ng2-charts';
 @Component({
   selector: 'app-chart-bar',
   templateUrl: './chart-bar.component.html',
@@ -7,10 +8,41 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class ChartBarComponent implements OnInit {
+  @Input() horizontal: boolean = false;
+  @Input() labels: string[] = [];
+  @Input() datasets: ChartDataset<"bar", number[]>[] = [];
+  @ViewChild(BaseChartDirective) chart: BaseChartDirective | undefined;
+
+  public barChartOptions: ChartConfiguration['options'] = {
+    responsive: true,
+    scales: {
+      x: {},
+      y: {
+        min: 10
+      }
+    },
+    plugins: {
+      legend: {
+        display: true,
+      },
+    }
+  };
+  public barChartType: ChartType = 'bar';
+
+  public barChartData: ChartData<'bar'> = {
+    labels: [],
+    datasets: []
+  };
 
   constructor() { }
 
   ngOnInit(): void {
+    if(this.horizontal) {
+      this.barChartOptions!.indexAxis = 'y';
+      this.barChartOptions!.scales!["y"]!.min = 0;
+    }
+    this.barChartData.labels = this.labels;
+    this.barChartData.datasets = this.datasets;
   }
 
 }
--- src/app/charts/pages/double-bars/double-bars.component.ts
+++ src/app/charts/pages/double-bars/double-bars.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ChartDataset } from 'chart.js';
 
 @Component({
   selector: 'app-double-bars',
@@ -7,6 +8,16 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class DoubleBarsComponent implements OnInit {
+  providersData: ChartDataset<'bar', number[]>[] = [
+    { data: [100, 200, 300, 400, 500], label: 'Seller A' },
+    { data: [50, 250, 30, 450, 200], label: 'Seller B' },
+  ];
+
+  providersLabels: string[] = ['2021', '2022', '2023', '2024', '2025'];
+
+  productData: ChartDataset<'bar', number[]>[] = [
+    { data: [200, 300, 400, 300, 100], label: 'Cars', backgroundColor: 'blue' },
+  ];
 
   constructor() { }
 
